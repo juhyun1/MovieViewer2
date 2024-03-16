87d02d8d3b82112b1d44ed4947abcf898b1d8cee
juhyun <delight6@gmail.com>
2024-03-17 08:28:39 +0900

    MainActivity에서 Repository Test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,11 @@ secrets {
 
 dependencies {
 
+    implementation(projects.core.data)
+    implementation(projects.core.domain)
+    implementation(projects.core.network)
+    implementation(projects.core.common)
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/com/movieviewer/MainActivity.kt
+++ app/src/main/java/com/movieviewer/MainActivity.kt
@@ -3,47 +3,64 @@ package com.movieviewer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.movieviewer.ui.theme.MovieViewerTheme
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 //        splashScreen.setKeepOnScreenCondition { true }
+        viewModel.createAccessToken()
         setContent {
             MovieViewerTheme {
-                // A surface container using the 'background' color from the theme
+                val list = viewModel.test.collectAsState(initial = null)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting("Android")
+                    list.value?.let {
+                        Timber.v("asdf: $list")
+                        LazyColumn {
+                            items(it.results) { movie ->
+                                Row {
+                                    Text(
+                                        text = "${movie.title}: ",
+                                        color = Color.Black,
+                                    )
+                                    Spacer(modifier = Modifier.width(3.dp))
+                                    Text(
+                                        text = movie.release_date,
+                                        color = Color.Black,
+                                    )
+//                                    Spacer(modifier = Modifier.width(3.dp))
+//                                    Text(text = movie.overview)
+                                }
+                                Spacer(modifier = Modifier.height(10.dp))
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieViewerTheme {
-        Greeting("Android")
     }
 }
