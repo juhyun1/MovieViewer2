fba8d70aa98dd5ada983511c42530beb2dc90fd7
juhyun <delight6@gmail.com>
2024-03-17 02:59:04 +0900

    Spotless 적용.

--- build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -5,17 +5,17 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
-  override fun apply(target: Project) {
-    with(target) {
-      with(pluginManager) {
-        apply("com.android.library")
-        apply("org.jetbrains.kotlin.android")
-      }
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
 
-      extensions.configure<LibraryExtension> {
-        configureKotlinAndroid(this)
-        defaultConfig.targetSdk = 34
-      }
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 34
+            }
+        }
     }
-  }
 }
--- build-logic/convention/src/main/java/com/movieviewer/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/com/movieviewer/KotlinAndroid.kt
@@ -5,45 +5,46 @@ import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionAware
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
 internal fun Project.configureKotlinAndroid(
-  commonExtension: CommonExtension<*, *, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
-  commonExtension.apply {
-    compileSdk = 34
+    commonExtension.apply {
+        compileSdk = 34
 
-    defaultConfig {
-      minSdk = 21
+        defaultConfig {
+            minSdk = 21
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+        }
+
+        lint {
+            abortOnError = false
+        }
+
+        kotlinOptions {
+            // Treat all Kotlin warnings as errors (disabled by default)
+            allWarningsAsErrors = properties["warningsAsErrors"] as? Boolean ?: false
+
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-opt-in=kotlin.RequiresOptIn",
+                // Enable experimental coroutines APIs, including Flow
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                // Enable experimental compose APIs
+                "-opt-in=com.google.accompanist.pager.ExperimentalPagerApi",
+                "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
+                "-opt-in=androidx.lifecycle.compose.ExperimentalLifecycleComposeApi",
+            )
+
+            // Set JVM target
+            jvmTarget = JavaVersion.VERSION_17.toString()
+        }
     }
-
-    compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_17
-      targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    lint {
-      abortOnError = false
-    }
-
-    kotlinOptions {
-      // Treat all Kotlin warnings as errors (disabled by default)
-      allWarningsAsErrors = properties["warningsAsErrors"] as? Boolean ?: false
-
-      freeCompilerArgs = freeCompilerArgs + listOf(
-        "-opt-in=kotlin.RequiresOptIn",
-        // Enable experimental coroutines APIs, including Flow
-        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-        // Enable experimental compose APIs
-        "-opt-in=com.google.accompanist.pager.ExperimentalPagerApi",
-        "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api",
-        "-opt-in=androidx.lifecycle.compose.ExperimentalLifecycleComposeApi",
-      )
-
-      // Set JVM target
-      jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-  }
 }
 
 fun CommonExtension<*, *, *, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
-  (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
 }
