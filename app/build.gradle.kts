bc7b1171165e0bc2ab9b47cfd6c63eeb0d80f90b
juhyun <delight6@gmail.com>
2024-03-17 04:04:47 +0900

    GoogleSecrets 적용하여 TMDB API KEY를 BuildConfig에 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("movie.android.application.compose")
     id("movie.android.hilt")
     id("movie.spotless")
+    id(libs.plugins.google.secrets.get().pluginId)
 }
 
 android {
@@ -16,11 +17,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
@@ -54,6 +50,13 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "secrets.defaults.properties"
+    ignoreList.add("keyToIgnore") // Ignore the key "keyToIgnore"
+    ignoreList.add("sdk.*") // Ignore all keys matching the regexp "sdk.*"
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -14,4 +14,5 @@ plugins {
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.android.test) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.google.secrets) apply false
 }
