fe0f31426638793b61eb3cbeb2847f51e60a025f
juhyun <delight6@gmail.com>
2024-03-17 09:31:28 +0900

    dto to domain model mapper 생성

---
+++ core/data/src/main/java/com/movieviewer/core/data/mapper/AuthMapper.kt
@@ -0,0 +1,25 @@
+package com.movieviewer.core.data.mapper
+
+
+import com.movieviewer.core.network.model.AccessToken
+import com.movieviewer.core.network.model.RequestToken
+import com.movieviewer.core.domain.model.AccessToken as DomainAccessToken
+import com.movieviewer.core.domain.model.RequestToken as DomainRequestToken
+fun AccessToken.asDomain(): DomainAccessToken {
+    return DomainAccessToken(
+        accountId = account_id,
+        accessToken = access_token,
+        success = success,
+        statusMessage = status_message,
+        statusCode = status_code,
+    )
+}
+
+fun RequestToken.asDomain(): DomainRequestToken {
+    return DomainRequestToken(
+       statusMessage = status_message,
+       requestToken = request_token,
+       success = success,
+       statusCode = status_code,
+    )
+}
---
+++ core/data/src/main/java/com/movieviewer/core/data/mapper/MovieListMapper.kt
@@ -0,0 +1,44 @@
+package com.movieviewer.core.data.mapper
+
+
+import com.movieviewer.core.domain.model.MovieListInfo
+import com.movieviewer.core.network.model.Dates
+import com.movieviewer.core.network.model.Movie
+import com.movieviewer.core.network.model.MovieListResponse
+import com.movieviewer.core.domain.model.Movie as DomainMovie
+import com.movieviewer.core.domain.model.Dates as DomainDates
+fun MovieListResponse.asDomain(): MovieListInfo {
+    return MovieListInfo(
+        page = page,
+        results = results.map { it.asDomain() },
+        dates = dates.asDomain(),
+        totalResults = total_results,
+        totalPages = total_pages,
+    )
+}
+
+fun Movie.asDomain(): DomainMovie {
+    return DomainMovie(
+        posterPath = poster_path,
+        adult = adult,
+        overview = overview,
+        releaseDate = release_date,
+        genreIds = genre_ids,
+        id = id,
+        originalTitle = original_title,
+        originalLanguage = original_language,
+        title = title,
+        backdropPath = backdrop_path,
+        popularity = popularity,
+        voteCount = vote_count,
+        video = video,
+        voteAverage = vote_average,
+    )
+}
+
+fun Dates.asDomain(): DomainDates {
+    return DomainDates(
+        maximum = maximum,
+        minimum = minimum
+    )
+}
