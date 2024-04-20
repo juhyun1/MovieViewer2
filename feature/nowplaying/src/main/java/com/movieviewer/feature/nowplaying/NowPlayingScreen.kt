5018572cca1a8f151eb8cb2816fef3487c9ab374
juhyun <delight6@gmail.com>
2024-04-21 00:26:13 +0900

    NowPlayingScreen을 MVVM에서 MVI 구조로 변경

--- feature/nowplaying/src/main/java/com/movieviewer/feature/nowplaying/NowPlayingScreen.kt
+++ feature/nowplaying/src/main/java/com/movieviewer/feature/nowplaying/NowPlayingScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
@@ -42,13 +43,17 @@ import com.movieviewer.core.domain.model.Movie
 import com.movieviewer.core.navigation.AppComposeNavigator
 import com.movieviewer.core.navigation.Screens
 import com.movieviewer.designsystem.theme.MovieViewerTheme
+import kotlinx.coroutines.flow.flow
+import org.orbitmvi.orbit.compose.collectAsState
 
 @Composable
 fun NowPlayingScreen(
     viewModel: NowPlayingViewModel = hiltViewModel(),
     composeNavigator: AppComposeNavigator,
 ) {
-    val moviePagingItems: LazyPagingItems<Movie> = viewModel.nowPlayingState.collectAsLazyPagingItems()
+    val moviePagingItems: LazyPagingItems<Movie> =
+        viewModel.nowPlayingState.collectAsLazyPagingItems()
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background,
@@ -57,75 +62,91 @@ fun NowPlayingScreen(
             viewModel.fetchNowPlayingList()
         }
 
-        LazyVerticalGrid(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(30.dp),
-            verticalArrangement = Arrangement.spacedBy(30.dp),
-            columns = GridCells.Fixed(2),
-        ) {
-            items(moviePagingItems.itemCount) {
-                val movie = moviePagingItems[it]
-                movie?.let {
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                composeNavigator.navigate(route = Screens.MovieDetails.createRoute(movieId = it.id))
-                            },
-                        colors = CardDefaults.cardColors(containerColor = MovieViewerTheme.colors.movieCard.background),
-                        elevation = CardDefaults.cardElevation(defaultElevation = 3.dp),
-                    ) {
-                        BoxWithConstraints {
-                            val maxWidth = this.maxWidth
-                            Column {
-                                AsyncImage(
-                                    model = ImageRequest.Builder(LocalContext.current)
-                                        .data(movie.posterPath.imagePath())
-                                        .crossfade(true)
-                                        .build(),
-                                    placeholder = painterResource(R.drawable.placeholder),
-                                    contentDescription = null,
-                                    contentScale = ContentScale.FillBounds,
-                                    modifier = Modifier
-                                        .fillMaxWidth()
-                                        .height(maxWidth * (270.dp / 180.dp)),
-                                )
-                                Spacer(modifier = Modifier.height(20.dp))
-                                Column(
-                                    modifier = Modifier
-                                        .padding(horizontal = 15.dp)
-                                        .height(70.dp),
-                                ) {
-                                    Text(
-                                        text = movie.title,
-                                        maxLines = 2,
-                                        overflow = TextOverflow.Ellipsis,
-                                        style = MovieViewerTheme.fonts.movieCard.title,
-                                        color = MovieViewerTheme.colors.movieCard.title,
-                                    )
-                                    Spacer(modifier = Modifier.height(5.dp))
-                                    Text(
-                                        text = movie.releaseDate,
-                                        style = MovieViewerTheme.fonts.movieCard.releaseDate,
-                                        color = MovieViewerTheme.colors.movieCard.releaseDate,
-                                    )
-                                }
-                                Spacer(modifier = Modifier.height(10.dp))
-                            }
-                            Box(
+        when {
+            moviePagingItems.loadState.refresh == LoadState.Loading || moviePagingItems.loadState.append == LoadState.Loading -> {
+                Text(text = "Loading")
+            }
+
+            moviePagingItems.loadState.refresh is LoadState.Error || moviePagingItems.loadState.append is LoadState.Error -> {
+                Text(text = "Error")
+            }
+
+            moviePagingItems.loadState.refresh is LoadState.NotLoading || moviePagingItems.loadState.append is LoadState.NotLoading -> {
+                LazyVerticalGrid(
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(30.dp),
+                    verticalArrangement = Arrangement.spacedBy(30.dp),
+                    columns = GridCells.Fixed(2),
+                ) {
+                    items(moviePagingItems.itemCount) {
+                        val movie = moviePagingItems[it]
+                        movie?.let {
+                            Card(
                                 modifier = Modifier
-                                    .padding(start = 10.dp, bottom = 85.dp)
-                                    .size(30.dp)
-                                    .clip(CircleShape)
-                                    .background(color = MovieViewerTheme.colors.movieCard.rate)
-                                    .align(alignment = Alignment.BottomStart),
-                                contentAlignment = Alignment.Center,
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        composeNavigator.navigate(
+                                            route = Screens.MovieDetails.createRoute(
+                                                movieId = it.id
+                                            )
+                                        )
+                                    },
+                                colors = CardDefaults.cardColors(containerColor = MovieViewerTheme.colors.movieCard.background),
+                                elevation = CardDefaults.cardElevation(defaultElevation = 3.dp),
                             ) {
-                                Text(
-                                    text = (movie.voteAverage * 10).toInt().toString(),
-                                    color = Color.White,
-                                    textAlign = TextAlign.Center,
-                                )
+                                BoxWithConstraints {
+                                    val maxWidth = this.maxWidth
+                                    Column {
+                                        AsyncImage(
+                                            model = ImageRequest.Builder(LocalContext.current)
+                                                .data(movie.posterPath.imagePath())
+                                                .crossfade(true)
+                                                .build(),
+                                            placeholder = painterResource(R.drawable.placeholder),
+                                            contentDescription = null,
+                                            contentScale = ContentScale.FillBounds,
+                                            modifier = Modifier
+                                                .fillMaxWidth()
+                                                .height(maxWidth * (270.dp / 180.dp)),
+                                        )
+                                        Spacer(modifier = Modifier.height(20.dp))
+                                        Column(
+                                            modifier = Modifier
+                                                .padding(horizontal = 15.dp)
+                                                .height(70.dp),
+                                        ) {
+                                            Text(
+                                                text = movie.title,
+                                                maxLines = 2,
+                                                overflow = TextOverflow.Ellipsis,
+                                                style = MovieViewerTheme.fonts.movieCard.title,
+                                                color = MovieViewerTheme.colors.movieCard.title,
+                                            )
+                                            Spacer(modifier = Modifier.height(5.dp))
+                                            Text(
+                                                text = movie.releaseDate,
+                                                style = MovieViewerTheme.fonts.movieCard.releaseDate,
+                                                color = MovieViewerTheme.colors.movieCard.releaseDate,
+                                            )
+                                        }
+                                        Spacer(modifier = Modifier.height(10.dp))
+                                    }
+                                    Box(
+                                        modifier = Modifier
+                                            .padding(start = 10.dp, bottom = 85.dp)
+                                            .size(30.dp)
+                                            .clip(CircleShape)
+                                            .background(color = MovieViewerTheme.colors.movieCard.rate)
+                                            .align(alignment = Alignment.BottomStart),
+                                        contentAlignment = Alignment.Center,
+                                    ) {
+                                        Text(
+                                            text = (movie.voteAverage * 10).toInt().toString(),
+                                            color = Color.White,
+                                            textAlign = TextAlign.Center,
+                                        )
+                                    }
+                                }
                             }
                         }
                     }
--- feature/nowplaying/src/main/java/com/movieviewer/feature/nowplaying/NowPlayingViewModel.kt
+++ feature/nowplaying/src/main/java/com/movieviewer/feature/nowplaying/NowPlayingViewModel.kt
@@ -9,23 +9,37 @@ import com.movieviewer.core.domain.usecase.MovieListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
 @HiltViewModel
 class NowPlayingViewModel @Inject constructor(
     private val movieListUseCase: MovieListUseCase,
-) : ViewModel() {
+) : ContainerHost<NowPlayingState, NowPlayingSideEffect>, ViewModel() {
+    override val container = container<NowPlayingState, NowPlayingSideEffect>(NowPlayingState())
+
     private val _nowPlayingState: MutableStateFlow<PagingData<Movie>> = MutableStateFlow(value = PagingData.empty())
     val nowPlayingState: MutableStateFlow<PagingData<Movie>> get() = _nowPlayingState
 
     fun fetchNowPlayingList() {
-        viewModelScope.launch {
-            movieListUseCase
-                .nowPlayingList()
-                .cachedIn(viewModelScope)
-                .collect {
-                    _nowPlayingState.value = it
-                }
+        intent {
+            viewModelScope.launch {
+                movieListUseCase
+                    .nowPlayingList()
+                    .cachedIn(viewModelScope)
+                    .collect {
+                        _nowPlayingState.value = it
+                    }
+            }
         }
     }
+}
+
+data class NowPlayingState(
+    val pagingData: PagingData<Movie> = PagingData.empty(),
+)
+
+sealed class NowPlayingSideEffect {
 }
